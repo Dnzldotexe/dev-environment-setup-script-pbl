302c87cbbd6cae6f54a3502782deb1baf5577d27
Danziel Cempron <104302220+Dnzldotexe@users.noreply.github.com>
2024-08-28 18:48:33 +0800

    Update dev-env-wasims.sh
    Start sail after installation

--- dev-env-wasims.sh
+++ dev-env-wasims.sh
@@ -15,6 +15,12 @@ cd $HOME
 curl -s "https://laravel.build/wasims?with=pgsql,redis,meilisearch,mailpit,selenium" | bash
 cd $HOME/wasims/
 
+# Run Sail in the background
+./vendor/bin/sail up -d
+
+# Run database migrations
+./vendor/bin/sail php artisan migrate
+
 # Install Tailwind CSS
 ./vendor/bin/sail npm install -D tailwindcss postcss autoprefixer
 ./vendor/bin/sail npx tailwindcss init -p
@@ -22,12 +28,6 @@ cd $HOME/wasims/
 # Install React using Laravel Breeze
 ./vendor/bin/sail php artisan breeze:install react
 ./vendor/bin/sail npm install
-
-# Run Sail in the background
-./vendor/bin/sail up -d
-
-# Run database migrations
-./vendor/bin/sail php artisan migrate
 
 # Compile assets
 ./vendor/bin/sail npm run dev
@@ -37,6 +37,7 @@ sudo echo "alias sail='./vendor/bin/sail'" >> ~/.bashrc
 source ~/.bashrc
 
 # Print some instructions
+echo ""
 echo "Sail is already running in the background, open your browser and type localhost"
 echo "Use commmand 'sail stop' to stop sail"
 echo "Use commmand 'sail down' to stop and remove sail"
